a41f959ecd4481b512f403ebb31152e10ecccc51
lingjinming <lingjinming@gsafety.com>
2020-08-17 20:44:29 +0800

    完善

--- api/index.js
+++ api/index.js
@@ -5,20 +5,30 @@ import { formatGetUri } from '@/common/util.js'
 // GET请求方式
 // api.register2 = params => request.globalRequest(`/mobile/signUp${formatGetUri(params)}`, 'GET',{}, 1)
 const api = {
+	// GET /mini/getUserInfo用户信息
+	getUserInfo:(params) => request.globalRequest(`/gas/mini/getUserInfo.mvc${formatGetUri(params)}`, 'GET',{}, 1),
+	
 	// 报警信息 request.globalRequest(`/gas/mini/getAlarmInfo`, 'GET',{}, 1)
 	getAlarmInfo:(params) => request.globalRequest(`/gas/mini/getAlarmInfo.mvc${formatGetUri(params)}`, 'GET',{}, 1),
+	
 	// 报警信息详情 request.globalRequest(`/gas/mini/alarmDetailInfo`, 'GET',{}, 1)
 	alarmDetailInfo:(params) => request.globalRequest(`/gas/mini/alarmDetailInfo.mvc${formatGetUri(params)}`, 'GET',{}, 1),
+	
 	// POST /gas/mini/saveAlarmRead保存已读状态
 	saveAlarmRead:(params) => request.globalRequest(`/gas/mini/saveAlarmRead.mvc${formatGetUri(params)}`, 'POST', {}, 1),
+	
 	// /gas/mini/relieveAlarm解除报警
 	relieveAlarm: (params) => request.globalRequest(`/gas/mini/relieveAlarm.mvc${formatGetUri(params)}`, 'GET',{}, 1),
+	
 	// GET /gas/mini/getEquipMonitor设备实时监测
 	getEquipMonitor:(params) => request.globalRequest(`/gas/mini/getEquipMonitor.mvc${formatGetUri(params)}`, 'GET',{}, 1),
+	
 	// GET /gas/mini/equipMonitorIndex.mvc设备运行指标
 	equipMonitorIndex:(params) => request.globalRequest(`/gas/mini/equipMonitorIndex.mvc${formatGetUri(params)}`, 'GET',{}, 1),
+	
 	// GET /gas/mini/getEquipDetailInfo.mvc设备监测详情
 	getEquipDetailInfo:(params) => request.globalRequest(`/gas/mini/getEquipDetailInfo.mvc${formatGetUri(params)}`, 'GET',{}, 1),
+	
 	//GET /mini/equipCurveData设备监测曲线 pipepointid
 	equipCurveData:(params) => request.globalRequest(`/gas/mini/equipCurveData.mvc${formatGetUri(params)}`, 'GET',{}, 1),
 	
--- pages/tabbar/user-center/user-center.vue
+++ pages/tabbar/user-center/user-center.vue
@@ -1,23 +1,45 @@
 <template>
-	<view>
-		个人中心
+	<view class="container"  :style="{'background-image': `url(${urlConfig}/gas/mini/getLocalFile/bg)`}">
+			<button plain @click="getUserInfo" open-type="getUserInfo"></button>
 	</view>
 </template>
 
 <script>
-	var _self;
-	export default {
-		data() {
-			return {
-			}
-		},
-		onLoad() {
-			_self = this;
-		},
-		methods: {}
+import {mapActions, mapState} from 'vuex'
+import urlConfig from '../../../common/config.js'
+let vm ;
+export default {
+	data() {
+		return {
+			urlConfig,
+		};
+	},
+	watch:{},
+	onLoad() {
+		vm = this
+		vm.getUserInfo()
+	},
+	methods: {
+		...mapActions(['setUserInfo']),
+		getUserInfo(){
+			uni.getUserInfo({
+			    complete: (res) => {
+			        console.log(res)
+					vm.setUserInfo(res.userInfo)
+			    }
+			})
+		}
 	}
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.container{
+	background-size: 100% 400rpx;
+	background-position: top center;
+	background-repeat: no-repeat;
+}
+button{
+	display: none;
+}
 </style>
