fd02bbe6f99ebf657d4b292c17c231b91de7f278
lingjinming <lingjinming@gsafety.com>
2020-09-10 19:54:34 +0800

    完善

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,308 +1,313 @@
 <template>
-	<view class="container"  :style="{'background-image': `url(${urlConfig}/gas/mini/getLocalFile/bg)`}">
-	<popup-layer v-show='boolShow' ref="popupRef" :direction="'top'">
-		  <button plain @getuserinfo='getuserinfo' class="btn_getuserinfo" open-type="getUserInfo" >
-			  请授权获取用户信息
-		  </button>
-	</popup-layer>
-	  <view class="header">
-		  <view class="title flex_center_row">燃气安全监测</view>
-		  <view class="flex_between_row">
-			  <view class="tab_box flex_between_row">
-				  <view class="act" v-for="item in btns"
-				  :key='item.id'
-				  :class="{'cur':curType == item.type}"
-				  @click="changeType(item.type)">{{item.type}}</view>
-			  </view>
-			  <image src="../../static/img/map.png" mode="aspectFit" @click="enterMap"></image>
-		  </view>
-	  </view>
-	  <view class="filter_box box_shadow">
-		  <template v-if="curType == '报警列表'">
-			  <type-tab :title="'报警类型'" :tabs.sync="alarmTypes">
-			  </type-tab>
-			  <type-tab :title="'报警状态'" :tabs.sync="alarmStatus">
-			  </type-tab>
-		  </template>
-		  <template v-else>
-			  <type-tab :title="'是否安装'" :tabs.sync="installStatus">
-			  </type-tab>
-			  <type-tab :title="'运行状态'" :tabs.sync="runStatus">
-			  </type-tab>
-		  </template>
-	  </view>
-	  <view class="search_box flex_between_row">
-		  <view>共<text>{{recordsNum}}</text>
-			{{curType == '报警列表'?'起报警':'台设备'}}
-		  </view>
-		  <view @click="requestSubscribeMessageFn" class="subscribe_btn">报警推送<switch :checked='isSubscribed' disabled="true"/></view>
-		  <!-- 暂时隐藏搜索功能 -->
-		  <!-- <view @click="enterSearch">
-			<image src="../../static/img/search.png" mode=""></image>
-		  </view> -->
-	  </view>
-		<scroll-view scroll-y="true" :style="{height:curType == '报警列表'?'calc(100% - 690rpx)':'calc(100% - 690rpx)'}">
-			<image class="no_alarm" :src='`${urlConfig}/gas/mini/getLocalFile/no_alarm`' v-if="!recordsNum"></image>
+	<view class="container" :style="{ 'background-image': `url(${urlConfig}/gas/mini/getLocalFile/bg)` }">
+		<popup-layer v-show="boolShow" ref="popupRef" :direction="'top'">
+			<button plain @getuserinfo="getuserinfo" class="btn_getuserinfo" open-type="getUserInfo">请授权获取用户信息</button>
+		</popup-layer>
+		<view class="header">
+			<view class="title flex_center_row">燃气安全监测</view>
+			<view class="flex_between_row">
+				<view class="tab_box flex_between_row">
+					<view class="act" v-for="item in btns" :key="item.id" :class="{ cur: curType == item.type }" @click="changeType(item.type)">{{ item.type }}</view>
+				</view>
+				<image src="../../static/img/map.png" mode="aspectFit" @click="enterMap"></image>
+			</view>
+		</view>
+		<view class="filter_box box_shadow">
 			<template v-if="curType == '报警列表'">
-				<monitor-item v-for='item in monitorData' :monitorData.sync='item' :key='item.alarmId'></monitor-item>
+				<type-tab :title="'报警类型'" :tabs.sync="alarmTypes"></type-tab>
+				<type-tab :title="'报警状态'" :tabs.sync="alarmStatus"></type-tab>
 			</template>
 			<template v-else>
-				<realtime-monitor-item v-for='item in equipMonitorData' :realtimeMonitorData.sync='item' :key='item.alarmId'></realtime-monitor-item>
+				<type-tab :title="'是否安装'" :tabs.sync="installStatus"></type-tab>
+				<type-tab :title="'运行状态'" :tabs.sync="runStatus"></type-tab>
+			</template>
+		</view>
+		<view class="search_box flex_between_row">
+			<view>
+				共
+				<text>{{ recordsNum }}</text>
+				{{ curType == '报警列表' ? '起报警' : '台设备' }}
+			</view>
+			<view @click="requestSubscribeMessageFn" class="subscribe_btn">
+				报警推送
+				<switch :checked="isSubscribed" disabled="true" />
+			</view>
+			<!-- 暂时隐藏搜索功能 -->
+			<!-- <view @click="enterSearch">
+			<image src="../../static/img/search.png" mode=""></image>
+		  </view> -->
+		</view>
+		<scroll-view scroll-y="true" :style="{ height: curType == '报警列表' ? 'calc(100% - 690rpx)' : 'calc(100% - 690rpx)' }">
+			<image class="no_alarm" :src="`${urlConfig}/gas/mini/getLocalFile/no_alarm`" v-if="!recordsNum"></image>
+			<template v-if="curType == '报警列表'">
+				<monitor-item v-for="item in monitorData" :monitorData.sync="item" :key="item.alarmId"></monitor-item>
+			</template>
+			<template v-else>
+				<realtime-monitor-item v-for="item in equipMonitorData" :realtimeMonitorData.sync="item" :key="item.alarmId"></realtime-monitor-item>
 			</template>
 		</scroll-view>
 	</view>
 </template>
 
 <script>
-import urlConfig from '../../common/config.js'
-import {mapActions, mapState} from 'vuex'
-import {mixin} from '../../common/mixins.js'
-let vm ;
+import urlConfig from '../../common/config.js';
+import { mapActions, mapState } from 'vuex';
+import { mixin } from '../../common/mixins.js';
+let vm;
 export default {
 	data() {
 		return {
 			urlConfig,
-			curType:'报警列表',
-			btns:[
+			curType: '报警列表',
+			btns: [
 				{
-					id:0,
-					type:'报警列表'
+					id: 0,
+					type: '报警列表'
 				},
 				{
-					id:1,
-					type:'实时监测'
+					id: 1,
+					type: '实时监测'
 				}
 			],
-			alarmTypes:[
-					{	
-						id:1,
-						value:'一级报警',
-						checked:true
-					},
-					{	
-						id:2,
-						value:'二级报警',
-						checked:true
-					},
-					{
-						id:3,
-						value:'三级报警',
-						checked:true
-					}
-			],
-			alarmStatus:[
-					{	
-						id:0,
-						value:'未解除',
-						checked:true
-					},
-					{	
-						id:1,
-						value:'已解除',
-						checked:false
-					}
-			],
-			installStatus:[
+			alarmTypes: [
 				{
-					id:0,
-					value:'未安装',
-					checked:true
+					id: 1,
+					value: '一级报警',
+					checked: true
 				},
-				{	
-					id:1,
-					value:'已安装',
-					checked:true
-				}
-			],
-			runStatus:[
 				{
-					id:0,
-					value:'设备异常',
-					checked:true
+					id: 2,
+					value: '二级报警',
+					checked: true
 				},
-				{	
-					id:1,
-					value:'正常运行',
-					checked:true
+				{
+					id: 3,
+					value: '三级报警',
+					checked: true
 				}
 			],
-			level:'1,2,3',
-			ravelflag:"0",
-			install:"0,1",
-			status:"0,1",
-			recordsNum:0,
-			monitorData:[],
-			equipMonitorData:[]
+			alarmStatus: [
+				{
+					id: 0,
+					value: '未解除',
+					checked: true
+				},
+				{
+					id: 1,
+					value: '已解除',
+					checked: false
+				}
+			],
+			installStatus: [
+				{
+					id: 0,
+					value: '未安装',
+					checked: true
+				},
+				{
+					id: 1,
+					value: '已安装',
+					checked: true
+				}
+			],
+			runStatus: [
+				{
+					id: 0,
+					value: '设备异常',
+					checked: true
+				},
+				{
+					id: 1,
+					value: '正常运行',
+					checked: true
+				}
+			],
+			level: '1,2,3',
+			ravelflag: '0',
+			install: '0,1',
+			status: '0,1',
+			recordsNum: 0,
+			monitorData: [],
+			equipMonitorData: []
 		};
 	},
-	mixins:[mixin],
+	mixins: [mixin],
 	onShow() {
-		vm = this
+		vm = this;
 		// vm.isAcceptedFn()  //判断当前是否打开了订阅功能
-		
-		if(!vm.userInfo || !vm.userInfo.nickName){
-			vm.boolShow = true
+
+		if (!vm.userInfo || !vm.userInfo.nickName) {
+			vm.boolShow = true;
 			vm.$refs.popupRef.show(); // 或者 boolShow = rue
-			vm.getuserinfo()
-		}else{
-			vm.changeType(vm.curType)
-			vm.boolShow = false
+			vm.getuserinfo();
+		} else {
+			vm.changeType(vm.curType);
+			vm.boolShow = false;
 			vm.$refs.popupRef.close();
 		}
-		
 	},
 	mounted() {
 		// 监听 报警解除成功事件
-		uni.$on('relieveAlarmSuccess',()=>{
-			vm.changeType(this.curType)
-		})
+		uni.$on('relieveAlarmSuccess', () => {
+			vm.changeType(this.curType);
+		});
 	},
 	computed: {
 		...mapState({
-			userInfo: ({user}) => user.userInfo,
-			tmplIds: ({tmplate}) => tmplate.tmplIds,
-			isSubscribed: ({tmplate}) => tmplate.isSubscribed,
+			userInfo: ({ user }) => user.userInfo,
+			tmplIds: ({ tmplate }) => tmplate.tmplIds,
+			isSubscribed: ({ tmplate }) => tmplate.isSubscribed
 		})
 	},
-	watch:{
-		alarmTypes:{
-			handler(newVal){
-				this.transformArrToStr(newVal,'level')
-				this.getAlarmInfoFn()
+	watch: {
+		alarmTypes: {
+			handler(newVal) {
+				this.transformArrToStr(newVal, 'level');
+				this.getAlarmInfoFn();
 			},
-			deep:true
+			deep: true
 		},
-		alarmStatus:{
-			handler(newVal){
-				this.transformArrToStr(newVal,'ravelflag')
-				this.getAlarmInfoFn()
+		alarmStatus: {
+			handler(newVal) {
+				this.transformArrToStr(newVal, 'ravelflag');
+				this.getAlarmInfoFn();
 			},
-			deep:true
+			deep: true
 		},
-		installStatus:{
-			handler(newVal){
-				this.transformArrToStr(newVal,'install')
-				this.getEquipMonitorFn()
+		installStatus: {
+			handler(newVal) {
+				this.transformArrToStr(newVal, 'install');
+				this.getEquipMonitorFn();
 			},
-			deep:true
+			deep: true
 		},
-		runStatus:{
-			handler(newVal){
-				this.transformArrToStr(newVal,'status')
-				this.getEquipMonitorFn()
+		runStatus: {
+			handler(newVal) {
+				this.transformArrToStr(newVal, 'status');
+				this.getEquipMonitorFn();
 			},
-			deep:true
+			deep: true
 		}
 	},
 	methods: {
-		...mapActions('user',['setUserInfo']),
-		getuserinfo(){// 小程序获取用户昵称头像等信息
+		...mapActions('user', ['setUserInfo']),
+		getuserinfo() {
+			// 小程序获取用户昵称头像等信息
 			uni.getUserInfo({
-			    success: (res) => {
-					vm.setUserInfo(res.userInfo) 
-					vm.getUserInfoFn(res.userInfo.nickName)
-			    }
-			})
+				success: res => {
+					vm.setUserInfo(res.userInfo);
+					vm.getUserInfoFn(res.userInfo.nickName);
+				}
+			});
 		},
-		getUserInfoFn(userName){// 接口获取用户信息
-			vm.$api.getUserInfo({
-				userName
-			}).then(res => {
-				uni.setStorage({
-					key:'userInfoByApi',
-					data:res.data,
-					success() {
-						vm.changeType(vm.curType)
-					}
+		getUserInfoFn(userName) {
+			// 接口获取用户信息
+			vm.$api
+				.getUserInfo({
+					userName
 				})
-				vm.boolShow = false
-				vm.$refs.popupRef.close();
-			})
+				.then(res => {
+					uni.setStorage({
+						key: 'userInfoByApi',
+						data: res.data,
+						success() {
+							vm.changeType(vm.curType);
+						}
+					});
+					vm.boolShow = false;
+					vm.$refs.popupRef.close();
+				});
 		},
-		reset(){
-			this.recordsNum = 0
-			this.monitorData = []
-			this.equipMonitorData = []
+		reset() {
+			this.recordsNum = 0;
+			this.monitorData = [];
+			this.equipMonitorData = [];
 		},
-		enterMap(){
+		enterMap() {
 			uni.navigateTo({
-				url:'../views/map'
-			})
+				url: '../views/map'
+			});
 		},
-		enterSearch(){
+		enterSearch() {
 			uni.navigateTo({
-				url:'../views/map'
-			})
+				url: '../views/map'
+			});
 		},
-		changeType(type){ // 判断当前是属于 报警列表还是实时监测
-			this.curType = type
-			this.reset()
-			if(this.curType == '报警列表'){
-				this.getAlarmInfoFn()
-			}else{
-				this.getEquipMonitorFn()
+		changeType(type) {
+			// 判断当前是属于 报警列表还是实时监测
+			this.curType = type;
+			this.reset();
+			if (this.curType == '报警列表') {
+				this.getAlarmInfoFn();
+			} else {
+				this.getEquipMonitorFn();
 			}
-			
 		},
-		getAlarmInfoFn(){ // 接口获取报警列表信息
-			this.$api.getAlarmInfo({
-				level:this.level,
-				ravelflag:this.ravelflag,
-				userName:vm.userInfo.nickName
-			}).then(res =>{
-				this.monitorData = res.data.data
-				this.recordsNum = this.monitorData.length
-			})
+		getAlarmInfoFn() {
+			// 接口获取报警列表信息
+			this.$api
+				.getAlarmInfo({
+					level: this.level,
+					ravelflag: this.ravelflag,
+					userName: vm.userInfo.nickName
+				})
+				.then(res => {
+					this.monitorData = res.data.data;
+					this.recordsNum = this.monitorData.length;
+				});
 		},
-		getEquipMonitorFn(){// 接口获取设备列表信息
-			this.$api.getEquipMonitor({
-				install:this.install,
-				userName:vm.userInfo.nickName,
-				runStatus:this.status
-			}).then(res =>{
-				this.equipMonitorData = res.data
-				this.recordsNum = this.equipMonitorData.length
-			})
-		},
+		getEquipMonitorFn() {
+			// 接口获取设备列表信息
+			this.$api
+				.getEquipMonitor({
+					install: this.install,
+					userName: vm.userInfo.nickName,
+					runStatus: this.status
+				})
+				.then(res => {
+					this.equipMonitorData = res.data;
+					this.recordsNum = this.equipMonitorData.length;
+				});
+		}
 	}
 };
 </script>
 
 <style lang="scss" scoped>
-	switch{
-		transform: scale(0.6);
-	}
-.container{
+switch {
+	transform: scale(0.6);
+}
+.container {
 	background-size: 100% 400rpx;
 	background-position: top center;
 	background-repeat: no-repeat;
 }
-.header{
+.header {
 	position: relative;
 	color: #fff;
 	margin: 0 30rpx 20rpx;
-	.flex_between_row{
+	.flex_between_row {
 		height: 70rpx;
 	}
-	.title{
+	.title {
 		position: relative;
 		height: 50px;
 		font-size: 20px;
 		margin-top: 60rpx;
 		// margin-bottom: 40rpx;
 	}
-	.tab_box{
+	.tab_box {
 		width: 280rpx;
-		.cur{
+		.cur {
 			position: relative;
 			font-size: 32rpx;
 			font-weight: 600;
-			&::after{
+			&::after {
 				position: absolute;
 				content: '';
 				width: 40rpx;
 				height: 4rpx;
-				background: #FFFFFF;
+				background: #ffffff;
 				border-radius: 4rpx;
 				left: 50%;
 				bottom: -8rpx;
@@ -310,51 +315,51 @@ export default {
 			}
 		}
 	}
-	image{
-		width:50rpx;
+	image {
+		width: 50rpx;
 		height: 50rpx;
 	}
 }
-.filter_box{
+.filter_box {
 	margin: 0 30rpx;
 	// height: 340rpx;
 	padding: 20rpx 30rpx 10rpx;
 	border-radius: 10rpx;
-	background: #FFFFFF;
+	background: #ffffff;
 	box-sizing: border-box;
 	display: flex;
 	flex-wrap: wrap;
 	// align-items: center;
 }
-type-tab{
+type-tab {
 	margin-bottom: 30rpx;
 }
-.search_box{
+.search_box {
 	position: relative;
 	margin: 0 30rpx;
 	height: 80rpx;
 	flex-shrink: 0;
-	view:first-child{
+	view:first-child {
 		color: #666;
 		font-size: 24rpx;
 	}
-	view:last-child{
+	view:last-child {
 		color: $uni-color-primary;
-		color:#666;
+		color: #666;
 		display: flex;
 		align-items: center;
 	}
-	text{
+	text {
 		font-size: 32rpx;
 		color: $uni-color-primary;
 		margin: 0 6rpx;
 	}
-	image{
+	image {
 		width: 30rpx;
 		height: 30rpx;
-		margin:0 10rpx;
+		margin: 0 10rpx;
 	}
-	.subscribe_btn{
+	.subscribe_btn {
 		position: relative;
 		right: -20rpx;
 		border-radius: 10rpx;
@@ -364,15 +369,15 @@ type-tab{
 		// color: #fff !important;
 	}
 }
-scroll-view{
-	.no_alarm{
+scroll-view {
+	.no_alarm {
 		width: 64%;
 		height: 330rpx;
 		margin-left: 14%;
 		margin-top: 100rpx;
 	}
 }
-.btn_getuserinfo{
+.btn_getuserinfo {
 	height: 100rpx;
 	line-height: 100rpx;
 }
--- pages/views/map.vue
+++ pages/views/map.vue
@@ -1,263 +1,252 @@
 <template>
 	<view class="container">
-	<map class="map" id="map" ref="map"
-	:longitude="longitude" 
-	:latitude="latitude" :scale="scale" 
-	:markers="newMarkers"
-	@markertap='chooseMarker'
-	show-location>
-	</map> 	
-	<view class="layer_box" @click='show'>
-			<image src="../../static/img/layer.png"></image>
-	</view>
-	<uni-search-bar :placeholder="'搜地点,查设备,找窨井'" @confirm='getkeyword' @clear='getkeyword'></uni-search-bar>
-	<popup-layer v-show='boolShow' ref="popupRef" :direction="'left'">
-	  <type-tab :title="'报警级别'" :isColumn=true :tabs.sync="alarmTypes">
-	  </type-tab>
-	</popup-layer>
-	
-	<monitor-item-detail v-if='monitorDetail' ref='monitorDetail'></monitor-item-detail>
-	<realtime-monitor-item-detail v-if='showRealtimeMonitorDetail' ref='showRealtimeMonitorDetail'></realtime-monitor-item-detail>
-	  
+		<map class="map" id="map" ref="map" :longitude="longitude" :latitude="latitude" :scale="scale" :markers="newMarkers" @markertap="chooseMarker" show-location></map>
+		<view class="layer_box" @click="show"><image src="../../static/img/layer.png"></image></view>
+		<uni-search-bar :placeholder="'搜地点,查设备,找窨井'" @confirm="getkeyword" @clear="getkeyword"></uni-search-bar>
+		<popup-layer v-show="boolShow" ref="popupRef" :direction="'left'"><type-tab :title="'报警级别'" :isColumn="true" :tabs.sync="alarmTypes"></type-tab></popup-layer>
+
+		<monitor-item-detail v-if="monitorDetail" ref="monitorDetail"></monitor-item-detail>
+		<realtime-monitor-item-detail v-if="showRealtimeMonitorDetail" ref="showRealtimeMonitorDetail"></realtime-monitor-item-detail>
 	</view>
 </template>
 
 <script>
-let amapFile = require('../../libs/amap-wx.js');  //引入高德js
-let vm ;
-let mapContext ;
-const deviceCheckedIconPath = `../../static/img/alarm/device_checked.png` //设备选中后图标
-const deviceIconPath = `../../static/img/alarm/device_alarm.png`
+let amapFile = require('../../libs/amap-wx.js'); //引入高德js
+let vm;
+let mapContext;
+const deviceCheckedIconPath = `../../static/img/alarm/device_checked.png`; //设备选中后图标
+const deviceIconPath = `../../static/img/alarm/device_alarm.png`;
 export default {
 	data() {
 		return {
-			fromMapEnter:false,//默认从列表进入的地图
-			scale:10,
-			showRealtimeMonitorDetail:false, // 默认不展示监测详情
-			monitorDetail:false,// 默认不展示设备详情
-			key:'9e794c3f3803c391c37b1a815f05a504',
+			fromMapEnter: false, //默认从列表进入的地图
+			scale: 10,
+			showRealtimeMonitorDetail: false, // 默认不展示监测详情
+			monitorDetail: false, // 默认不展示设备详情
+			key: '9e794c3f3803c391c37b1a815f05a504',
 			markers: [],
 			newMarkers: [],
 			latitude: '',
 			longitude: '',
 			textData: {},
-			boolShow:false,
-			alarmTypes:[
-					{	
-						id:1,
-						value:'一级报警',
-						checked:true
-					},
-					{	
-						id:2,
-						value:'二级报警',
-						checked:true
-					},
-					{
-						id:3,
-						value:'三级报警',
-						checked:true
-					},
-					{
-						id:0,
-						value:'无报警',
-						checked:true
-					}
-			],
+			boolShow: false,
+			alarmTypes: [
+				{
+					id: 1,
+					value: '一级报警',
+					checked: true
+				},
+				{
+					id: 2,
+					value: '二级报警',
+					checked: true
+				},
+				{
+					id: 3,
+					value: '三级报警',
+					checked: true
+				},
+				{
+					id: 0,
+					value: '无报警',
+					checked: true
+				}
+			]
 		};
 	},
-	watch:{
-		markers(newVal){
-			this.newMarkers = this.unique(newVal,'id')
+	watch: {
+		markers(newVal) {
+			this.newMarkers = this.unique(newVal, 'id');
 		},
-		alarmTypes:{
-			handler(newVal){
-				vm = this
-				let arr = []
-				 newVal.forEach(item => {
-					 if (item.checked) {
-						 arr.push(item.id)
-					 }
-				 })
-				this.level = arr.join(',')
-				if(arr.length == vm.alarmTypes.length){
-					this.level = ''		 
+		alarmTypes: {
+			handler(newVal) {
+				vm = this;
+				let arr = [];
+				newVal.forEach(item => {
+					if (item.checked) {
+						arr.push(item.id);
+					}
+				});
+				this.level = arr.join(',');
+				if (arr.length == vm.alarmTypes.length) {
+					this.level = '';
 				}
-				this.initMap()
+				this.initMap();
 			},
-			deep:true,
-			immediate:true,
-		},
+			deep: true,
+			immediate: true
+		}
 	},
 	onLoad(options) {
 		vm = this;
-		if(options.realtimeMonitorDetail){
-			this.showRealtimeMonitorDetail = true
-			this.fromMapEnter = false
-		}else if(options.monitorDetail){
-			this.monitorDetail = true
-			this.fromMapEnter = false
-		}else{
-			this.fromMapEnter = true
-			uni.getLocation({ // 默认定位到用户位置
-			    type: 'gcj02',
-			    success: function (res) {
-					vm.longitude = res.longitude
-					vm.latitude = res.latitude
-			    }
+		if (options.realtimeMonitorDetail) {
+			this.showRealtimeMonitorDetail = true;
+			this.fromMapEnter = false;
+		} else if (options.monitorDetail) {
+			this.monitorDetail = true;
+			this.fromMapEnter = false;
+		} else {
+			this.fromMapEnter = true;
+			uni.getLocation({
+				// 默认定位到用户位置
+				type: 'gcj02',
+				success: function(res) {
+					vm.longitude = res.longitude;
+					vm.latitude = res.latitude;
+				}
 			});
 		}
 	},
-	mounted(){
-		mapContext = uni.createMapContext('map', this)
-		
-		uni.$on('alarmDetailPos',(alarmDetailPos)=>{
-			let data = alarmDetailPos[0]
-			vm.scale = 16
+	mounted() {
+		mapContext = uni.createMapContext('map', this);
+
+		uni.$on('alarmDetailPos', alarmDetailPos => {
+			let data = alarmDetailPos[0];
+			vm.scale = 16;
 			mapContext.moveToLocation({
 				longitude: data.longtitude,
 				latitude: data.latitude
-			})
-			if(!vm.fromMapEnter){
-				vm.markers = []
+			});
+			if (!vm.fromMapEnter) {
+				vm.markers = [];
 			}
-			
-			let iconPath = `../../static/img/alarm/alarm_level${data.alarmLevel.substring(0,1)}.png`
+
+			let iconPath = `../../static/img/alarm/alarm_level${data.alarmLevel.substring(0, 1)}.png`;
 			vm.markers.push({
-				id:data.markerId,
+				id: data.markerId,
 				latitude: data.latitude || '',
 				longitude: data.longtitude || '',
 				iconPath,
 				width: 40,
 				height: 40,
-				alarmId:data.alarmId || '',
-				equipId:data.equipId || '',
-				alarmLevel:data.alarmLevel || '',
-			})
-		})
-		uni.$on('realtimeMonitorDetail',(data)=>{
-			vm.scale = 16
+				alarmId: data.alarmId || '',
+				equipId: data.equipId || '',
+				alarmLevel: data.alarmLevel || ''
+			});
+		});
+		uni.$on('realtimeMonitorDetail', data => {
+			vm.scale = 16;
 			mapContext.moveToLocation({
 				longitude: data.longtitude,
 				latitude: data.latitude
-			})
-			
-			if(!vm.fromMapEnter){
-				vm.markers = []
+			});
+
+			if (!vm.fromMapEnter) {
+				vm.markers = [];
 			}
 			vm.markers.push({
-				id:data.markerId,
+				id: data.markerId,
 				latitude: data.latitude || '',
 				longitude: data.longtitude || '',
-				iconPath:deviceCheckedIconPath,
+				iconPath: deviceCheckedIconPath,
 				width: 40,
 				height: 40,
-				alarmId:data.alarmId || '',
-				equipId:data.equipId || '',
-				alarmLevel:data.alarmLevel || '',
-			})
-		})
+				alarmId: data.alarmId || '',
+				equipId: data.equipId || '',
+				alarmLevel: data.alarmLevel || ''
+			});
+		});
 	},
 	beforeDestroy() {
-		uni.$off('alarmDetailPos')
-		uni.$off('realtimeMonitorDetail')
+		uni.$off('alarmDetailPos');
+		uni.$off('realtimeMonitorDetail');
 	},
 	methods: {
 		unique(arr, attrName) {
 			const res = new Map();
-			return arr.filter((a) => !res.has(a[attrName]) && res.set(a[attrName], 1));
+			return arr.filter(a => !res.has(a[attrName]) && res.set(a[attrName], 1));
 		},
-		getkeyword(data){
-			vm.scale = 10
-			vm.monitorDetail = false
-			vm.showRealtimeMonitorDetail = false
-			vm.$api.getGlobalSearchInfo({
-				name:data.value,
-				userName:vm.$store.state.user.userInfo.nickName
-			}).then(res=>{
-				vm.fromMapEnter = true
-				vm.showMarkers(res.data.data)
-			})
+		getkeyword(data) {
+			vm.scale = 10;
+			vm.monitorDetail = false;
+			vm.showRealtimeMonitorDetail = false;
+			vm.$api
+				.getGlobalSearchInfo({
+					name: data.value,
+					userName: vm.$store.state.user.userInfo.nickName
+				})
+				.then(res => {
+					vm.fromMapEnter = true;
+					vm.showMarkers(res.data.data);
+				});
 		},
-		show(){
-			this.boolShow = true
+		show() {
+			this.boolShow = true;
 			this.$refs.popupRef.show(); // 或者 boolShow = rue
 		},
-		close(){
-			this.$refs.popupRef.close();// 或者 boolShow = false
+		close() {
+			this.$refs.popupRef.close(); // 或者 boolShow = false
 		},
-		chooseMarker(e){
-
-			vm.scale = 16
-			let curMarker = vm.markers.filter(item => item.id == e.detail.markerId)
-			if(this.markers.length == 1){
-				return false
+		chooseMarker(e) {
+			vm.scale = 16;
+			let curMarker = vm.markers.filter(item => item.id == e.detail.markerId);
+			if (this.markers.length == 1) {
+				return false;
 			}
-			this.markers.forEach(item =>{
-				item['width'] = 30
-				item['height'] = 30
-				if(item.alarmId == ''){
-					item['iconPath'] = deviceIconPath
+			this.markers.forEach(item => {
+				item['width'] = 30;
+				item['height'] = 30;
+				if (item.alarmId == '') {
+					item['iconPath'] = deviceIconPath;
 				}
-			})
-			curMarker[0]['width'] = 40
-			curMarker[0]['height'] = 40
-			if(curMarker[0].alarmId == ''){ // 设备点位
-				this.showRealtimeMonitorDetail = true
-				this.monitorDetail = false
-				curMarker[0]['iconPath'] = deviceCheckedIconPath
+			});
+			curMarker[0]['width'] = 40;
+			curMarker[0]['height'] = 40;
+			if (curMarker[0].alarmId == '') {
+				// 设备点位
+				this.showRealtimeMonitorDetail = true;
+				this.monitorDetail = false;
+				curMarker[0]['iconPath'] = deviceCheckedIconPath;
 				uni.setStorage({
-					key:'realtimeMonitorDetail',
-					data:curMarker[0],
+					key: 'realtimeMonitorDetail',
+					data: curMarker[0],
 					success() {
-						vm.$refs.showRealtimeMonitorDetail.init()
+						vm.$refs.showRealtimeMonitorDetail.init();
 					}
-				})
-				
-			}else{
-				this.monitorDetail = true
-				this.showRealtimeMonitorDetail = false
-				
-				uni.setStorage({
-					key:'monitorDetail',
-					data:curMarker[0],
-					success() {
-						vm.$refs.monitorDetail.init()
-					}
-				})
-		
-			}
+				});
+			} else {
+				this.monitorDetail = true;
+				this.showRealtimeMonitorDetail = false;
 
+				uni.setStorage({
+					key: 'monitorDetail',
+					data: curMarker[0],
+					success() {
+						vm.$refs.monitorDetail.init();
+					}
+				});
+			}
 		},
-		initMap(){
-			vm.scale = 10
-			vm.$api.getAlarmMapView({
-				level:vm.level,
-				userName:vm.$store.state.user.userInfo.nickName
-			}).then(res => {
-				vm.showMarkers(res.data.data)
-			})
+		initMap() {
+			vm.scale = 10;
+			vm.$api
+				.getAlarmMapView({
+					level: vm.level,
+					userName: vm.$store.state.user.userInfo.nickName
+				})
+				.then(res => {
+					vm.showMarkers(res.data.data);
+				});
 		},
-		showMarkers(markersArr){
-			vm.markers = []
-			markersArr.forEach((item,index) => {
-				let iconPath = item.alarmid ? `../../static/img/alarm/alarm_level${item.alarmLevel}.png`:
-				`../../static/img/alarm/device_alarm.png`
+		showMarkers(markersArr) {
+			vm.markers = [];
+			markersArr.forEach((item, index) => {
+				let iconPath = item.alarmid ? `../../static/img/alarm/alarm_level${item.alarmLevel}.png` : `../../static/img/alarm/device_alarm.png`;
 				// let title = '甲烷浓度超标报警'
 				// debugger
 				vm.markers.push({
-				  // title,
-				  id:item.markerId,
-				  latitude: item.latitude || '',
-				  longitude: item.longtitude || '',
-				  iconPath,
-				  width: 30,
-				  height: 30,
-				  alarmId:item.alarmId || '',
-				  equipId:item.equipId || '',
-				  alarmLevel:item.alarmLevel || '',
-				})
-			})
+					// title,
+					id: item.markerId,
+					latitude: item.latitude || '',
+					longitude: item.longtitude || '',
+					iconPath,
+					width: 30,
+					height: 30,
+					alarmId: item.alarmId || '',
+					equipId: item.equipId || '',
+					alarmLevel: item.alarmLevel || ''
+				});
+			});
 			// console.log(vm.markers)
 		}
 	}
@@ -265,26 +254,25 @@ export default {
 </script>
 
 <style lang="scss">
-.map{
-  width: 100%;
-  height: 100%;
+.map {
+	width: 100%;
+	height: 100%;
 }
-popup-layer view{
+popup-layer view {
 	width: 40vw;
 }
-.layer_box{
+.layer_box {
 	position: absolute;
 	right: 26rpx;
 	top: 200rpx;
 	height: 100rpx;
 	width: 100rpx;
-	image{
+	image {
 		width: 100%;
 		height: 100%;
 	}
-
 }
-uni-search-bar{
+uni-search-bar {
 	position: absolute;
 	top: 40rpx;
 	left: 40rpx;
@@ -293,5 +281,4 @@ uni-search-bar{
 	border-radius: 10rpx;
 	box-shadow: 0 0 10rpx 8rpx rgba(211, 211, 211, 0.2);
 }
-
 </style>
